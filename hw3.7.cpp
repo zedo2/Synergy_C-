e8d253286ba7b569fec910d5c806ee4896edc163
zedo2 <88265539+zedo2@users.noreply.github.com>
2023-01-22 21:44:35 +0300

    HW 3.7

---
+++ hw3.7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+main() {	
+	int sum=0;
+    for(int i=1; i<101; i++) {
+		sum+=i;
+	}
+	cout << "Summa chisel ot 1 do 100 = ";
+	cout << sum << '\n';
+}
+
