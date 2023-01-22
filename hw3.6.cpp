9c348166e2da157f252e6ea7d07da7d296cc97dd
zedo2 <88265539+zedo2@users.noreply.github.com>
2023-01-22 22:16:26 +0300

    Add files via upload

--- hw3.6.cpp
+++ hw3.6.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
+
+using namespace std;
 
 main() {
 	for(int i=1; i<11; i++) {
-		std::cout << i << ' ';
+		cout << i << ' ';
 		printf("* na 5 = ");
-		std::cout << i*5 << '\n';
+		cout << i*5 << '\n';
 	}
+	
+	int x=7;
+	int y, i;
+	
+	do {
+		cout << "vvedite chislo x\n";
+		cin >> y;
+	    if(x<y) {
+	        cout << y << ' ';
+	        printf(" bolshe chisla x");
+		}
+		if(x>y) {
+	        cout << y << ' ';
+	        printf(" menshe chisla x");
+		}
+		if(x==y) {
+			cout << y << ' ';
+        	printf(" eto chislo x\ngame over");
+        	exit(EXIT_SUCCESS);
+		}
+	}
+	while(y!=x);
 }
 
