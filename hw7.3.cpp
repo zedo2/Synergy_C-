9ba6756c577ddde4da0b1ce6ca68086dbe69d337
zedo2 <88265539+zedo2@users.noreply.github.com>
2023-02-20 22:22:25 +0300

    Add files via upload

--- hw7.3.cpp
+++ hw7.3.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 
-int squareInt(int n);
-void tripleFunc(int[]);
+int squareInt(int);
+int squareInt(int);
+int squareInt(int*);
  
 int main() {
 	int arr[10];
@@ -12,7 +13,14 @@ int main() {
 		cout << "vvedite " << i + 1 << " chislo ";
 		cin >> arr[i];	  
 	}
-	tripleFunc(arr);
+	int n = arr[1];
+	int &nRef = n;
+	int t = arr[2];
+	
+	squareInt(arr[0]);
+	squareInt(nRef);
+	squareInt(t);
+	
 	return 1;    
 }
 
@@ -30,27 +38,9 @@ int squareInt(int n) {
 			cout << n << " - net, eto ne koren celogo chisla\n";
 		}		
 	}
+	
 }
-int squareInt() {
-	int n;
-	cout << "vvedite chislo ";
-	cin >> n;
-	if (n == 0) {
-		printf("nol ne podhodit");
-	} 
-	int i2=0;
-    for(int i = 2; i2 < n; i++) {  
-        i2 = i * i;
-        if(i2 == n){
-        	cout << n << " - da, eto koren celogo chisla " << n / i << "\n";
-        	return n;
-		} 
-		if(i2 > n) {
-			cout << n << " - net, eto ne koren celogo chisla\n";
-			return 0;
-		}		
-	}
-}
+
 int squareInt(int *n) {
 	if (n == 0) {
 		printf("nol ne podhodit");
@@ -66,10 +56,4 @@ int squareInt(int *n) {
 		}		
 	}
 	return *n;
-}
-
-void tripleFunc(int arr[]) {
-	squareInt(arr[0]);
-	squareInt(arr[1]);
-	squareInt(arr[2]);
 }
